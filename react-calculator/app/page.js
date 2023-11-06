5a3348b02c67d8bf8727fb2b24d6259ff8847598
GhassanKY <elkhouryghassan712@gmail.com>
2023-11-06 22:51:48 +0100

    feat: :tada: V 1.0

---
+++ react-calculator/app/page.js
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Calculadora Creada en REACT</h1>
+      
+    </main>
+  )
+}
